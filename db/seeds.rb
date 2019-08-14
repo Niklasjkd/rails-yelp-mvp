6db572534b43a5f5bef5aa033aa38abd36bad860
Niklas Düttmann <niklas.duettmann033@gmail.com>
2019-08-14 14:29:02 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all restaurants"
+Restaurant.destroy_all
+
+puts "Cresting new restaurants"
+
+restaurants = [
+  {
+    name: "Brasil Brasileiro Churrascaria Rodizio",
+    address: "Kurfuerstendamm 151, 10709 Berlin Deutschland",
+    phone_number: "49 30 89409607",
+    category: "italian"
+  },
+  {
+    name: "Blockhouse",
+    address: "Kurfuerstendamm 161, 10709 Berlin Deutschland",
+    phone_number: "49 30 8919355",
+    category: "italian"
+  },
+  {
+    name: "Goldhorn Beefclub",
+    address: "Mommsenstrasse 12 10629 Berlin, 10629 Berlin ",
+    phone_number: "49 30 31806370",
+    category: "japanese"
+  },
+  {
+    name: "Steakhouse ASADOR",
+    address: "Wilhelmstrasse 22 10963 Berlin, 10963 Berlin ",
+    phone_number: "+49 30 25931818",
+    category: "italian"
+  },
+  {
+    name: "Beef Grill Club by Hasir",
+    address: "KLeipziger Platz 12, 10117 Berlin Deutschland",
+    phone_number: "49 30 20644390",
+    category: "belgian"
+  },
+]
+
+Restaurant.create!(restaurants)
